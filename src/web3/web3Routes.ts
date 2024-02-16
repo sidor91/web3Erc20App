37fc126011bb2655ff6a82f0ebb1b7776eaef4e5
Serhii Sydorenko <sidorchik91@gmail.com>
2024-02-16 08:49:56 +0200

    ADD | web3Routes

---
+++ src/web3/web3Routes.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { controllerWrapper } from "../decorators/controllerWrapper";
+import { sendTransactionController, getUserBalance } from "./web3Controllers";
+import { authenticate, isValidErc20TokenAddress } from "../middlewares";
+
+const router = express.Router();
+
+router.post("/transfer", authenticate, isValidErc20TokenAddress, controllerWrapper(sendTransactionController));
+
+router.get(
+	"/balance/:token_addr/:user_addr",
+	authenticate,
+	isValidErc20TokenAddress,
+	controllerWrapper(getUserBalance)
+);
+
+export default router;
